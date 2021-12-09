359b552eec8cab616a07eb24345255cb408e33f9
Dana Bradley <dbradley0909@outlook.com>
2021-12-08 23:52:15 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+/*
+first block cmnt
+scnd block cmnt
+//third cmnt
+
+ */
+    public static void main(String[] args) {
+        //th is a stand alone cmnt
+	System.out.println("first line from app");// this is a line cmnt
+   // System.out.println("Second line from app");
+    System.out.println(/*"Third line from app"*/"different text");
+
+    }
+}
